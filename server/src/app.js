94c7f3be0579efff8961f50b9fc273dc35990ec7
Sayed Shehata <sayedshehata33@gmail.com>
2023-06-24 19:29:03 +0300

    feat: :package: Apply logging requests by morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const morgan = require("morgan");
 const cors = require("cors");
 // express code
 const express = require("express");
@@ -13,6 +13,9 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public")));
