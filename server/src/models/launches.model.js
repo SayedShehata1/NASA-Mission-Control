1cf32ee4f2ce5be38b8f967922798622a3e7d1c1
Sayed Shehata <sayedshehata33@gmail.com>
2023-07-02 19:13:09 +0300

    feat: :sparkles: Add pagination endpoints

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -9,19 +9,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 0;
 
-const launch = {
-  flightNumber: 100, // flight_number
-  mission: "kepler Edxploration X", // name
-  rocket: "Explor IS1", // rocket.name
-  launchDate: new Date("December 27, 2030"), // data_local
-  target: "kepler-442 b", // not applicable
-  customers: ["ZTM", "NASA"], // payload.customers for each payload
-  upcoming: true, // upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
 async function populateLaunches() {
@@ -106,8 +93,12 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
