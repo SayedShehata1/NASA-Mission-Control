624808712d5802cbc60b0e13f736dd1a7156b8fd
Sayed Shehata <sayedshehata33@gmail.com>
2023-07-02 16:52:23 +0300

    feat: :alien: Load SpaceX api data in the app

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,8 @@ const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
+const { loadLaunchData } = require("./models/launches.model");
+
 // take the port that we specify in json file start or take 8000
 const PORT = process.env.PORT || 8000;
 
@@ -16,6 +18,7 @@ async function startServer() {
   // Connecting to MongoDB
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
